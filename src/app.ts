49e0ded2539b064082375aa2a3d8c272578c32df
Davi <davi@MBP-de-Diana.lan>
2023-08-21 09:15:42 -0300

    Creating route with POSTMAN

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 // console.log('Express + TS!')
 
+// - Init Express
 import express from 'express';
 
 const app = express()
 
+// - rota com POST
+app.use(express.json());
+
 app.get("/", (req, res) => {
     return res.send("Hello Express!")
+})
+
+// rota com POST
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+
+    return res.send("Product added");
 })
 
 app.listen(3000, () => {
