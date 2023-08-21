74324326fd53c974686dc94db8952ed80f277d47
Davi <davi@MBP-de-Diana.lan>
2023-08-21 09:28:20 -0300

    Creating routes for all methods

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,22 @@ app.post("/api/product", (req, res) => {
     console.log(req.body)
 
     return res.send("Product added");
+});
+
+// - rota para todos os verbos
+
+app.all("/api/product/check", (req, res) => {
+
+    // req.method = VERBO HTTP
+
+    if(req.method === "POST") {
+        return res.send("Inserted any record")
+    } else if(req.method === "GET") {
+        return res.send("Read any record")
+    } else {
+        return res.send("We cannot perform this operation")
+    }
+
 })
 
 app.listen(3000, () => {
