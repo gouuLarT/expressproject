a0273a61f8a27aa4d453eb194f7c2bd33a6835b9
Davi <davi@MBP-de-Diana.lan>
2023-08-22 11:33:18 -0300

    Req e Res with generics

--- src/app.ts
+++ src/app.ts
@@ -116,6 +116,21 @@ app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
     return res.json({msg: "Welcome to admin space" });
 })
 
+// - Req e Res com generics
+
+app.get("/api/user/:id/details/:name", 
+    (
+    req: Request<{ id:string; name:string }>, 
+    res: Response<{ status: boolean }>
+    ) => {
+        console.log(`ID: ${req.params.id}`)
+        console.log(`Name: ${req.params.name}`)
+
+        return res.json({ status: true })
+    }
+);
+
+
 
 app.listen(3000, () => {
     console.log("Express + TS app is working!");        
